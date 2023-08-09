ccf8bda76755767737c20214f04f4d19ed4cf4a2
SimpleSalad <SimpleSalad@users.noreply.github.com>
2023-08-09 02:36:05 -0400

    Add files via upload
    cog test 1

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,14 +1,23 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from discord.app_commands import Choice
 
-@app_commands.check(is_owner)
-@app_commands.command(name = "shutdown", description = "Shuts down the bot.")
-async def shutdown(self, interaction: discord.Interaction):
-    embed = discord.Embed(title = "", description = "Incredible!")
-    message = interaction.message
-    await interaction.response.send_message(embed = embed)
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def is_owner(interaction: discord.Interaction) -> bool:
+        return interaction.user.id == 88087496189022208
     
-    print("\nShutdown by command.")
-    await self.bot.close()
+    @app_commands.check(is_owner)
+    @app_commands.command(name = "shutdown", description = "Stops the bot.")
+    async def shutdown(self, interaction: discord.Interaction):
+        embed = discord.Embed(title = "Shutting down.", description = "이건 몰랐을걸? Incredible!")
+        message = interaction.message
+        await interaction.response.send_message(embed = embed)
+    
+        print("\nShutdown by command.")
+        await self.bot.close()
+
+async def setup(bot: commands.Bot) -> None:
+  await bot.add_cog(Admin(bot))
--- troubleshooter.py
+++ troubleshooter.py
@@ -1,28 +1,49 @@
+from os.path import dirname, basename, isfile
 import os
+import sys
+import importlib
 import discord
+import asyncio
 
 from dotenv import load_dotenv
+from discord import app_commands
 from discord.ext import commands
 
 load_dotenv()
-
 TOKEN = os.getenv('BOT_TOKEN')
-intents = discord.Intents.default()
-intents.message_content = True
 
-bot = commands.Bot(intents=intents,
-                   command_prefix='fn$',
-                   owner_id = 88087496189022208,
-                   status = discord.Status.do_not_disturb,
-                   activity = discord.Game(name = "Dungeon Fighter Online", type = discord.ActivityType.competing))
+class troubleshooter(commands.Bot):
+    def __init__(self) -> None:
+        intents = discord.Intents.all()
+        command_prefix = "fn$"
+        super().__init__(command_prefix=command_prefix,
+                        intents = intents,
+                         owner_id = 88087496189022208,
+                         status = discord.Status.do_not_disturb,
+                         activity = discord.Game(name = "Dungeon Fighter Online", type = discord.ActivityType.custom))
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+    async def setup_hook(self):
+        imported_cogs = 0
+        total_cogs = 0
+        
+        cogs = os.path.join(os.path.dirname(__file__), 'cogs/')
+        for cog in os.listdir(cogs):
+            cog_name = os.fsdecode(cog)
+            if cog_name != "__init__.py" and cog_name.endswith(".py"):
+                total_cogs += 1
+                try:
+                    await self.load_extension(f"cogs.{cog_name[:-3]}")
+                    imported_cogs += 1
+                except Exception as error:
+                    print(f"X: {cog_name} could not be loaded ({type(error).__name__}: {error})")
+        print(f"\n{total_cogs} cog(s) found. {imported_cogs} cog(s) successfully loaded.")
+        
+        if imported_cogs == 0:
+            print("Nothing loaded, terminating bot.")
+            sys.exit()
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} connected.')
+    async def on_ready(self):
+        print(f'{bot.user.name} connected.')
     
-
+bot = troubleshooter()
 bot.run(TOKEN)
