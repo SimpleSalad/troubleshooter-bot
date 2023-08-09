306d1ff4e045368ca3fcac9ec7d8032b328b3949
SimpleSalad <SimpleSalad@users.noreply.github.com>
2023-08-09 15:56:42 -0400

    Add files via upload

--- cogs/utils/randomazzo.py
+++ cogs/utils/randomazzo.py
@@ -1,12 +1,20 @@
 import random
 import json
+from pathlib import Path
 
 import os
 
-class Flavor():
-    def __init__(self):
-        self.flavor = {
-            "desc": "",
-            "img": "",
-            "footer": ""
-        }
+class Flavor_Text():
+    def __init__(self, flavor_type, flavor_index):
+        self.flavor_type = flavor_type
+        self.flavor_index = flavor_index
+    
+    def get_flavor(self)
+        json_filepath = str(Path(os.path.dirname(__file__)).parents[0]) + "/_json/flavor.json"
+        
+        with open(json_filepath) as f
+            data = json.load(f)
+    
+class Flavor_Boom(Flavor_Text):
+    def __init__(self, result):
+        super().__init__(
